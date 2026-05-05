6c311ae730511a05aaf02c2e20590e96e958f9a1
gamzenurkalay78-web <gamzenurkalay78@gmail.com>
2026-05-05 19:46:01 +0300

    Add MaterialApp setup in ReverseTabuApp

--- main.dart
+++ main.dart
@@ -7,3 +7,15 @@ void main() => runApp(const ReverseTabuApp());
 
 class ReverseTabuApp extends StatelessWidget {
   const ReverseTabuApp({super.key});
+
+    @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reverse Tabu',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: const GameScreen(),
+    );
+  }
+}
+
