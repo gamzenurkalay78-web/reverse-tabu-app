6865e7f29234ea6f059b7222e18926536a439ccc
gamzenurkalay78-web <gamzenurkalay78@gmail.com>
2026-05-05 19:44:51 +0300

    Initialize Flutter app with ReverseTabuApp

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+
+void main() => runApp(const ReverseTabuApp());
+
+
+class ReverseTabuApp extends StatelessWidget {
+  const ReverseTabuApp({super.key});
